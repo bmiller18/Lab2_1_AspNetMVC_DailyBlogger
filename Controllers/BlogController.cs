4ab3d6aec5daa8cd38bb7b9f79700fa34f5a99b9
Brittany Miller <bmiller012712@gmail.com>
2020-08-04 16:29:00 -0400

    Third Version

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -40,5 +40,34 @@ namespace Lab2_1_AspNetMVC_DailyBlogger.Controllers
             }
             return View(blog);
         }
+        public IActionResult Details(int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+        }
+        public IActionResult Edit(int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([Bind("blogTitle,content,blogDate,id")] BlogPost blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+            return View(blog);
+        }
+        public IActionResult Delete([Bind("id")] int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            _context.Remove(blogPost);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(List));
+        }
     }
 }
